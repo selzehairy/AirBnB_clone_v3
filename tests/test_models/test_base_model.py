6ef2fa9ff6f90759ad7065ef33a692fa81ddfd27
selzehairy <selzehairy@gmail.com>
2023-07-30 07:55:48 -0700

    2 air

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -87,11 +87,13 @@ class TestBaseModel(unittest.TestCase):
         time.sleep(1e-4)
         toc = datetime.now()
         self.assertTrue(tic <= inst1.created_at <= toc)
+
         tic = datetime.now()
         inst2 = BaseModel()
         time.sleep(1e-4)
         tic = datetime.now()
         self.assertTrue(tic <= inst2.created_at <= toc)
+
         self.assertEqual(inst1.created_at, inst1.updated_at)
         self.assertEqual(inst2.created_at, inst2.updated_at)
         self.assertNotEqual(inst1.created_at, inst2.created_at)
